77aeba4bc7890493428d920c88e8692ca8938271
Allie <alliekim@hawaii.edu>
2016-03-20 16:34:01 -1000

    Added functions for table

--- table.h
+++ table.h
@@ -11,3 +11,7 @@ typedef struct {
 	TableData data[MAXSIZE];
 	int size;
 } Table;
+
+void initTable(Table * table);
+void insert(Table * table, int valid, char destaddr, int out);
+void displayTable(Table * table);
